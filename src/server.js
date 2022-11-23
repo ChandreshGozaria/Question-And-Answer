8b1ffb163073166f8c0ccfa80996e29de3a315ec
ChandreshThakor <chandresht95@gmail.com>
2022-11-23 23:07:04 +0530

    passport.js work

--- src/server.js
+++ src/server.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
 const port = process.env.PORT || 3000;
 
+dotenv.config();
 
 // Middeleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: false}))
+
+//Database connection 
+mongoose.connect(`${process.env.database}`);
+
+// User registion 
+app.post('/api/registion', async(req, res) => {
+    try {
+        const hashPassword = await bcrypt.hash(req.body.password , 10);
+        console.log(req.body);
+    
+        res.send(req.body)
+    } catch (error) {
+        
+    }
+})
 
 app.get('/api/getQuestion', (req, res) => {
     req.params
